11d982423d9f6b4919130eea62aa8494019b009e
Felipe Rodrigues <felipersdf@hotmail.com>
2019-07-28 16:13:30 -0300

    Adicionando projeto laravel

---
+++ forum-project/app/Thread.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Thread extends Model
+{
+  //
+
+  public function replies()
+  {
+      return $this->hasMany(Reply::class);
+  }
+}
---
+++ forum-project/database/seeds/ReplySeeder.php
@@ -0,0 +1,15 @@
+<?php
+use Illuminate\Database\Seeder;
+use App\Reply;
+class ReplySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // factory(Reply::class, 50)->create();
+    }
+}
