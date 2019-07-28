eb06d8e10db9aac9a7e0c71a686ba645ce4e4956
Felipe Rodrigues <felipersdf@hotmail.com>
2019-07-28 16:41:43 -0300

    Um usuario pode criar uma thread

--- forum-project/app/Http/Controllers/ThreadController.php
+++ forum-project/app/Http/Controllers/ThreadController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 
 class ThreadController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth')->only('store');
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -28,15 +33,14 @@ class ThreadController extends Controller
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+        $thread = Thread::create([
+            'user_id' => auth()->id(),
+            'title' => request('title'),
+            'body' => request('body')
+        ]);
+        return redirect($thread->path());
     }
 
     /**
